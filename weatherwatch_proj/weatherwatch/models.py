058ccc2674a92ab86d1bfb222e5de2d9dba42e65
Julius Stopforth <jjstopforth@users.noreply.github.com>
2014-11-14 02:58:09 +0200

    initial db setip and project halt

--- weatherwatch_proj/weatherwatch/models.py
+++ weatherwatch_proj/weatherwatch/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Station(models.Model):
+
+    stationID = models.CharField(max_length=128, unique=True)
+
+    def __unicode__(self):
+        return self.stationID
--- weatherwatch_proj/weatherwatch_proj/settings.py
+++ weatherwatch_proj/weatherwatch_proj/settings.py
@@ -7,6 +7,7 @@ PROJECT_PATH = os.path.join(SETTINGS_DIR, os.pardir)
 PROJECT_PATH = os.path.abspath(PROJECT_PATH)
 TEMPLATES_PATH = os.path.join(PROJECT_PATH, 'templates')
 STATIC_PATH = os.path.join(PROJECT_PATH,'static')
+DATABASE_PATH = os.path.join(PROJECT_PATH, 'weatherwatch.db')
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -19,13 +20,13 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': DATABASE_PATH,                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
-        'USER': '',
-        'PASSWORD': '',
-        'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-        'PORT': '',                      # Set to empty string for default.
+        #'USER': '',
+        #'PASSWORD': '',
+        #'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        #'PORT': '',                      # Set to empty string for default.
     }
 }
 
